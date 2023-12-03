31badcc55e10be29a6f4116261b7d57d3ad21dc0
AlexCam11 <alexcaml0@gmail.com>
2023-12-02 19:18:15 -0500

    Nuevo Login

--- src/components/Sales/Inicio.jsx
+++ src/components/Sales/Inicio.jsx
@@ -3,8 +3,8 @@ import chartImg from "../../assets/img/images/grafico-final.png";
 import SalesTabButton from "./SalesTabButton";
 import SalesTabContent from "./SalesTabContent";
 import repr from "../../assets/img/logo/logoMus.png";
-import imag1 from "../../assets/img/logo/login free.png";
-import imag2 from "../../assets/img/logo/login vip.png";
+import imag1 from "../../assets/img/logo/PIAbotonfree.png";
+import imag2 from "../../assets/img/logo/PIAbotonvip.png";
 
 const Inicio = () => {
   const chart_info_list = [
@@ -19,17 +19,20 @@ const Inicio = () => {
   return (
     <section id="reproductor" className="chart-area chart-bg2 ">
       <div className="container">
-        <div className="row  justify-content-center">
+        <div className="row mt-5">
+        </div>
+        <div className="row  justify-content-center mt-5">
             <div className="col-md-8">
             <img src={repr}  />
             </div>
        
         </div>
-        <div className="row justify-content-center mt-5">
-          <div className="col-md-3" >
-         <a href="https://musiclogin.nexas.cloud/auth/login?next=/"><img src ={imag1} className="mt-2"/></a>
+        <div className="row justify-content-between mt-5 ">
+     
+          <div className="col-md-6 text-end" >
+         <a href="https://musiclogin.nexas.cloud/auth/login?next=/"><img src ={imag1} /></a>
           </div>
-          <div className="col-md-3">
+          <div className="col-md-5">
           <a href="https://musiclogin.nexas.cloud/auth/login?next=/"><img src ={imag2}/></a>
           </div>
         </div>
